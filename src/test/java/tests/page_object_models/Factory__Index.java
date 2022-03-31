14860cb08601fcd41286217ed133173083d2000f
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-03-31 14:20:44 -0400

    updated url props (there are technically 2 diff home urls I named 1 for the landing page and the other that says 'home' in the url as the Home pages' URL). Also added a method to test the navbar logo on hopefully each page. Haven't tested it yet on all pages but I assume it'll work.

--- src/test/java/tests/page_object_models/Factory__Index.java
+++ src/test/java/tests/page_object_models/Factory__Index.java
@@ -3,6 +3,7 @@ package tests.page_object_models;
 // SELENIUM
 import org.openqa.selenium.WebElement;
 // CUSTOM
+import org.openqa.selenium.support.FindBy;
 import utils.Asserts;
 import utils.Drivers;
 import utils.Waits;
@@ -21,13 +22,26 @@ import utils.Waits;
  * </ul>
  */
 public abstract class Factory__Index {
+
     // PROPS
+
     private final String pageUrl;
-    private final String pageHomeUrl = "http://www.leafground.com/";
+    private final String pageHomeUrl = "http://www.leafground.com/home.html";
+    private final String pageLandingPageUrl = "http://www.leafground.com/";
 
     // CONSTRUCTOR
+
     public Factory__Index(String webPageUrl) {
         pageUrl = webPageUrl;
+    }
+
+    // LOCATORS & GETTERS
+
+    @FindBy (css = "img[alt='logo Testleaf']")
+    public WebElement navbarLogo;
+
+    public WebElement getNavbarLogo() {
+        return navbarLogo;
     }
 
     /**
@@ -50,6 +64,30 @@ public abstract class Factory__Index {
      */
     public String getUrl() {
         return this.pageUrl;
+    }
+
+    /**
+     * Get this site's Home URL {@link "http://www.leafground.com/"}
+     * @return String : the URL
+     */
+    public String getUrlHome() {
+        return this.pageHomeUrl;
+    }
+
+    /**
+     * Test for verifying the Logo in the Navbar properly brings the user back to the Home page.
+     * @param thisPagesLink Web Element : The link on the Home page that navigate to this.pageUrl
+     */
+    public void TestLogoToHomeButton(WebElement thisPagesLink) {
+        // go to home page
+        Drivers.getDriver().navigate().to(this.pageLandingPageUrl);
+        // click link that navigates to this.pageUrl and assert
+        Asserts.navigation_fromElement_toUrl(thisPagesLink, this.pageUrl);
+        // wait for and click the logo in the navbar
+        Waits.forElement_andClick(this.navbarLogo);
+        // assert we made it back to the Home page
+        Asserts.navigation_toUrl(this.pageHomeUrl);
+
     }
 }
 
