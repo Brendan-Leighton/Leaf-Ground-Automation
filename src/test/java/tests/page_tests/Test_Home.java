2d7b324522a59f1404bd56f6afa79f3ed579cd2a
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-03-31 10:36:03 -0400

    added test for navigating to the Edit page from the home page

--- src/test/java/tests/page_tests/Test_Home.java
+++ src/test/java/tests/page_tests/Test_Home.java
@@ -3,13 +3,21 @@ package tests.page_tests;
 import org.testng.annotations.Test;
 import tests.page_object_models.Factory_Home;
 import tests.page_object_models._Init_Factories;
+import utils.Asserts;
 import utils.Drivers;
+import utils.Urls;
 
 public class Test_Home extends _Base_Test {
     private Factory_Home HOME = _Init_Factories.getHome();
 
-    @Test
+    @Test (priority = 0)
     public void HomePageLoads() {
         Drivers.getDriver().navigate().to(HOME.getUrl());
+    }
+
+    @Test (priority = 1)
+    public void CanNavigateFromHomeToEditPage() {
+        Drivers.getDriver().navigate().to(HOME.getUrl());
+        Asserts.navigation_fromElement_toUrl(HOME.getPageLink_edit(), Urls.get("edit"));
     }
 }
