12bd2533c5f4c1b6ea29bbd13e18bf17c014ab34
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-03-31 16:14:59 -0400

    added locators and getters

--- src/test/java/tests/page_object_models/Factory_Edit.java
+++ src/test/java/tests/page_object_models/Factory_Edit.java
@@ -2,6 +2,8 @@ package tests.page_object_models;
 
 // CUSTOM
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.Urls;
 
@@ -20,6 +22,41 @@ public class Factory_Edit extends Factory__Index {
 
     // LOCATORS
 
+    @FindBy (css = "input#email")
+    private WebElement inputEmail;
+
+    @FindBy (css = "input[value='Append ']")
+    private WebElement inputToAppendTo;
+
+    @FindBy (css = "input[value='TestLeaf']")
+    private WebElement inputToGetTextFrom;
+
+    @FindBy (css = "input[value='Clear me!!']")
+    private WebElement inputToClear;
+
+    @FindBy (css = "input[disabled='true']")
+    private WebElement inputToConfirmIsDisabled;
 
     // GETTERS
+
+
+    public WebElement getInputEmail() {
+        return inputEmail;
+    }
+
+    public WebElement getInputToAppendTo() {
+        return inputToAppendTo;
+    }
+
+    public WebElement getInputToGetTextFrom() {
+        return inputToGetTextFrom;
+    }
+
+    public WebElement getInputToClear() {
+        return inputToClear;
+    }
+
+    public WebElement getInputToConfirmIsDisabled() {
+        return inputToConfirmIsDisabled;
+    }
 }
