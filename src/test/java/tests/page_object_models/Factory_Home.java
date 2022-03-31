4767fec3eb7691edf713372667f728c06ea25904
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-03-31 10:35:36 -0400

    Update imports and added the new Urls class. Also added a locator and getter or the Edit page-link

--- src/test/java/tests/page_object_models/Factory_Home.java
+++ src/test/java/tests/page_object_models/Factory_Home.java
@@ -1,17 +1,25 @@
 package tests.page_object_models;
 // PARENT CLASS
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 // SELENIUM
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import utils.Props;
+import utils.Urls;
 
 public class Factory_Home extends Factory__Index {
 
     public Factory_Home(WebDriver driver) {
         // SUPER
-        super(Props.getProp("home"));
+        super(Urls.get("home"));
         // POM FACTORY
         PageFactory.initElements(driver, this);
+    }
+
+    @FindBy (css = "a[href='pages/Edit.html']")
+    public WebElement pageLink_edit;
+
+    public WebElement getPageLink_edit() {
+        return pageLink_edit;
     }
 }
