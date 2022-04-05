58f13dbf6a5e910d3052373d6f0cba0f56bcf8af
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-04-05 10:46:07 -0400

    Test 1 complete

--- src/test/java/tests/page_object_models/Factory_RadioButton.java
+++ src/test/java/tests/page_object_models/Factory_RadioButton.java
@@ -13,7 +13,9 @@ public class Factory_RadioButton extends Factory__Index {
 
     // LOCATORS
     @FindBy (css = "section.innerblock div:nth-child(1) >div>div input#yes")
-    private WebElement radioSelectAnAnswer;
+    private WebElement radioSelectAnAnswer_yes;
+    @FindBy (css = "section.innerblock div:nth-child(1) >div>div input#no")
+    private WebElement radioSelectAnAnswer_no;
 
     @FindBy (css = "section.innerblock div:nth-child(4) >div>div")
     private WebElement radioFindSelected;
@@ -23,7 +25,8 @@ public class Factory_RadioButton extends Factory__Index {
 
     // GETTERS
 
-    public WebElement getRadioSelectAnAnswer() { return radioSelectAnAnswer; }
+    public WebElement getRadioSelectAnAnswer_yes() { return radioSelectAnAnswer_yes; }
+    public WebElement getRadioSelectAnAnswer_no() { return radioSelectAnAnswer_no; }
     public WebElement getRadioFindSelected() { return radioFindSelected; }
     public WebElement getRadioPickOptionProgramatically() { return radioPickOptionProgramatically; }
 }
--- src/test/java/tests/page_tests/Test_RadioButton.java
+++ src/test/java/tests/page_tests/Test_RadioButton.java
@@ -1,10 +1,13 @@
 package tests.page_tests;
 
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import tests.page_object_models.Factory_Dropdown;
 import tests.page_object_models.Factory_RadioButton;
 import tests.page_object_models._Init_Factories;
+import utils.Waits;
 
 public class Test_RadioButton extends _Base_Test {
 
@@ -18,5 +21,22 @@ public class Test_RadioButton extends _Base_Test {
     @BeforeMethod
     public void GoToThisPage() {
         RADIO.navigateTo(HomePageLink);
+    }
+
+    /**
+     * Radio 1 - select a radio button
+     *
+     * Test 1 - select yes
+     */
+    @Test
+    public void VerifyOption_yes_IsSelectable() {
+        // SETUP
+        WebElement radioButton = RADIO.getRadioSelectAnAnswer_yes();
+
+        // INTERACT
+        Waits.forElement_andClick(radioButton);
+
+        // VERIFY
+        Assert.assertEquals(radioButton.getAttribute("selected"), "true");
     }
 }
