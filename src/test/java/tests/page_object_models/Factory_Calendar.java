566d6d1fb23d47b775c307789fec3a62353d6131
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-04-06 16:09:35 -0400

    initialized Factory_Calendar with locators

---
+++ src/test/java/tests/page_object_models/Factory_Calendar.java
@@ -0,0 +1,42 @@
+package tests.page_object_models;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import utils.Urls;
+
+public class Factory_Calendar extends Factory__Index {
+    public Factory_Calendar() {
+        super(Urls.get("calendar"));
+    }
+
+    // LOCATORS
+    @FindBy (css = "input#datepicker")
+    private WebElement calendarInput;
+
+    @FindBy (css = "div#ui-datepicker-div")
+    private WebElement calendarPopup;
+
+    @FindBy (css = "a[title='Next']")
+    private WebElement calendarButtonNextPage;
+
+    @FindBy (css = "table.ui-datepicker-calendar")
+    private WebElement calendarTable;
+
+    // GETTERS
+
+    public WebElement getCalendarInput() {
+        return calendarInput;
+    }
+
+    public WebElement getCalendarPopup() {
+        return calendarPopup;
+    }
+
+    public WebElement getCalendarButtonNextPage() {
+        return calendarButtonNextPage;
+    }
+
+    public WebElement getCalendarTable() {
+        return calendarTable;
+    }
+}
