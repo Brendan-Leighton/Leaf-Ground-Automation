10d25e8f6e155b9983cf6f8379b7a0baba80ac32
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-04-06 13:39:42 -0400

    added method to help count iframes for test 3

--- src/test/java/tests/page_object_models/Factory_Frames.java
+++ src/test/java/tests/page_object_models/Factory_Frames.java
@@ -2,9 +2,12 @@ package tests.page_object_models;
 // JAVA
 import java.util.List;
 // SELENIUM
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 // CUSTOM UTILS
+import utils.Drivers;
 import utils.Urls;
 
 
@@ -22,9 +25,9 @@ public class Factory_Frames extends Factory__Index {
     @FindBy (css = "section.innerblock > div#wrapframe > iframe")
     private List<WebElement> iframes;
 
-    private WebElement frameOne_clickButton;
-    private WebElement frameTwo_clickButtonInsideNestedFrame;
-    private WebElement frameThree_countFrames;
+    private final WebElement frameOne_clickButton;
+    private final WebElement frameTwo_clickButtonInsideNestedFrame;
+    private final WebElement frameThree_countFrames;
 
     // GETTERS
 
@@ -42,5 +45,30 @@ public class Factory_Frames extends Factory__Index {
 
     public WebElement getFrameThree_countFrames() {
         return frameThree_countFrames;
+    }
+
+    /**
+     * Recursively search for iframes
+     */
+    public int searchForIFrames() {
+        WebDriver driver = Drivers.getDriver();
+
+        // get all iframes
+        List<WebElement> iframes = driver.findElements(By.xpath("//iframe"));
+
+        // initialize count with iframes who are a direct descendant of the current element
+        int iFrameCount = iframes.size();
+
+        // determine if recursion is necessary
+        if (iframes.size() > 0) {
+            for (WebElement frame : iframes) {
+                driver.switchTo().frame(frame);
+                iFrameCount += searchForIFrames();
+                driver.switchTo().parentFrame();
+            }
+        }
+
+        // return count of all iframes who're descendants of this element
+        return iFrameCount;
     }
 }
