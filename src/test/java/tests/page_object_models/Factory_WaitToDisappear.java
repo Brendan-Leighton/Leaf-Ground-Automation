d97603d001273f4511dac2bb39609dff33967b91
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-04-07 17:02:24 -0400

    removed unecesary code

--- src/test/java/tests/page_object_models/Factory_WaitToDisappear.java
+++ src/test/java/tests/page_object_models/Factory_WaitToDisappear.java
@@ -14,11 +14,6 @@ public class Factory_WaitToDisappear extends Factory__Index {
     @FindBy(css = "button#btn")
     private WebElement buttonToWaitFor;
 
-    @FindBy(css = "button#btn > b")
-    private WebElement buttonText;
-    public String expectedButtonText = "I'm going to disapper.Keep looking at me !!";  // disappear typo in HTML
-
     // GETTERS
-
     public WebElement getButtonToWaitFor() { return buttonToWaitFor; }
 }
