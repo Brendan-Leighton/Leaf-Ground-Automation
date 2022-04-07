e4300772b6b1f3a5f54616beeb59ee7d57dacedc
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-04-07 10:38:04 -0400

    Sortable test complete

--- src/test/java/tests/page_tests/Test_Select_and_Sortable.java
+++ src/test/java/tests/page_tests/Test_Select_and_Sortable.java
@@ -1,5 +1,6 @@
 package tests.page_tests;
 // SELENIUM
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,6 +11,7 @@ import org.testng.annotations.BeforeMethod;
 // PAGE OBJECTS
 import org.testng.annotations.Test;
 import tests.page_object_models.Factory_Selectable;
+import tests.page_object_models.Factory_Sortable;
 import tests.page_object_models._Init_Factories;
 import utils.Drivers;
 import utils.Interacts;
@@ -20,20 +22,23 @@ import java.util.List;
 public class Test_Select_and_Sortable extends _Base_Test {
 
     // PROPS
-
+    // select
     private final Factory_Selectable SELECTABLE = _Init_Factories.getFactories().getSelectable();
-    private final WebElement HomePageLink = _Init_Factories.getHome().getPageLink_selectable();
+    private final WebElement HomePageLink_select = _Init_Factories.getHome().getPageLink_selectable();
+    // sort
+    private final Factory_Sortable SORTABLE = _Init_Factories.getFactories().getSortable();
+    private final WebElement HomePageLink_sort = _Init_Factories.getHome().getPageLink_sortable();
 
     // TESTS
 
-    @BeforeMethod
-    public void GoToThisPage() { SELECTABLE.navigateTo(HomePageLink); }
-
     /**
-     *  Test 1 - select multiple
+     *  Test 1 - Select - select multiple
      */
     @Test
     public void VerifySelectingMultiple() {
+        // NAVIGATE
+        SELECTABLE.navigateTo(HomePageLink_select);
+
         // SETUP
         WebDriver driver = Drivers.getDriver();
         List<WebElement> listEls = SELECTABLE.getSelectableList();
@@ -54,5 +59,48 @@ public class Test_Select_and_Sortable extends _Base_Test {
         Assert.assertTrue(listEls.get(4).getAttribute("class").contains("ui-selected"));
         Assert.assertFalse(listEls.get(5).getAttribute("class").contains("ui-selected"));
         Assert.assertTrue(listEls.get(6).getAttribute("class").contains("ui-selected"));
+    }
+
+    /**
+     *  Test 1 - Sort - sort items
+     */
+    @Test
+    public void VerifySortable() {
+        // NAVIGATE
+        SORTABLE.navigateTo(HomePageLink_sort);
+
+        // SETUP
+        WebDriver driver = Drivers.getDriver();
+        WebElement listEl = SORTABLE.getSelectableList();
+        Actions action = new Actions(driver);// get <li>
+        List<WebElement> listItemEls = listEl.findElements(By.cssSelector("li"));
+
+        // INTERACT
+        // move even numbers to the top
+        for (int i = 1; i < listItemEls.size(); i++) {
+            if (i % 2 == 1) {
+                // implement action
+                action.dragAndDrop(listItemEls.get(i), listItemEls.get(0)).perform();
+            }
+        }
+
+        // ASSERT
+        // get new order
+        List<WebElement> listItemEls_newOrder = listEl.findElements(By.cssSelector("li"));
+        // ("Item 2", "Item 4", etc.)
+        int[] expectedListOrder = {2, 4, 6, 1, 3, 5, 7};
+        // loop & assert
+        for (int i = 0; i < listItemEls_newOrder.size(); i++) {
+            Assert.assertTrue(listItemEls_newOrder.get(i).getText().contains(String.valueOf(expectedListOrder[i])));
+        }
+
+//        // VERIFY
+//        Assert.assertTrue(listEls.get(0).getAttribute("class").contains("ui-selected"));
+//        Assert.assertFalse(listEls.get(1).getAttribute("class").contains("ui-selected"));
+//        Assert.assertTrue(listEls.get(2).getAttribute("class").contains("ui-selected"));
+//        Assert.assertFalse(listEls.get(3).getAttribute("class").contains("ui-selected"));
+//        Assert.assertTrue(listEls.get(4).getAttribute("class").contains("ui-selected"));
+//        Assert.assertFalse(listEls.get(5).getAttribute("class").contains("ui-selected"));
+//        Assert.assertTrue(listEls.get(6).getAttribute("class").contains("ui-selected"));
     }
 }
