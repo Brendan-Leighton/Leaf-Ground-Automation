1b65fe443a776c30b9163caa02357a318468f574
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-04-07 15:35:10 -0400

    added file path data for the file that I'll be uploading

--- src/test/java/tests/page_object_models/Factory_Upload.java
+++ src/test/java/tests/page_object_models/Factory_Upload.java
@@ -13,7 +13,14 @@ public class Factory_Upload extends Factory__Index {
     @FindBy (css = "input[type=\"file\"]")
     private WebElement inputFile;
 
+    String uploadFilePath = "C:\\Users\\br3nd\\Pictures\\Memes\\KanyeMeme1.jpg";
+    String expectedFilePath = "C:\\fakepath\\KanyeMeme1.jpg";
+
     // GETTERS
 
     public WebElement getInputFile() { return inputFile; }
+
+    public String getUploadFilePath() { return uploadFilePath; }
+
+    public String getExpectedFilePath() { return expectedFilePath; }
 }
