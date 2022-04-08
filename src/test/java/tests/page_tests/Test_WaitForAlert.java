5bbaf6031663bc1059a7a2802050f5b85c16559a
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-04-08 09:29:41 -0400

    tests complete

--- src/test/java/tests/page_tests/Test_WaitForAlert.java
+++ src/test/java/tests/page_tests/Test_WaitForAlert.java
@@ -1,11 +1,19 @@
 package tests.page_tests;
 // SELENIUM
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 // TEST-NG
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 // PAGE MODELS
 import tests.page_object_models.Factory_WaitForAlert;
 import tests.page_object_models._Init_Factories;
+// CUSTOM UTILS
+import utils.Drivers;
+import utils.Interacts;
 
 public class Test_WaitForAlert extends _Base_Test {
 
@@ -19,5 +27,28 @@ public class Test_WaitForAlert extends _Base_Test {
     @BeforeMethod
     public void GoToThisPage() {
         ALERT.navigateTo(HomePageLink);
+    }
+
+    /**
+     * Test 1 - wait for and close alert
+     */
+    @Test
+    public void VerifyAlertShowsAndCloses() {
+        // SETUP
+        WebDriver driver = Drivers.getDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        WebElement button = ALERT.getButton();
+        String expectedAlertText = "Hurray, Click OK";
+
+        // INTERACT
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+        Interacts.click(button);
+        wait.until(ExpectedConditions.alertIsPresent());
+        String alertText = driver.switchTo().alert().getText().trim();
+        driver.switchTo().alert().accept();
+
+        // ASSERT
+        Assert.assertEquals(alertText, expectedAlertText);
+
     }
 }
